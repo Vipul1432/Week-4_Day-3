2b0e68180031c96d6905e619b0a3b765cd662778
Mr.Vipul <vipulupadhyay563@gmail.com>
2023-08-30 12:31:29 +0530

    Implement Route Resolver

--- RouteGuardPractical/src/app/app-routing.module.ts
+++ RouteGuardPractical/src/app/app-routing.module.ts
@@ -3,10 +3,17 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { AuthGuard } from './services/auth.guard';
+import { DataResolver } from './services/data.resolver';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
-  { path: 'home', component: HomeComponent, canActivate: [AuthGuard] },
+  { 
+    path: 'home',
+    component: HomeComponent,
+    resolve: { data: DataResolver },  
+    canActivate: [AuthGuard], 
+  },
+  { path: '', redirectTo: 'home', pathMatch: 'full' }
 ];
 
 @NgModule({
